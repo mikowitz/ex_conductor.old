b372efc4ef353063538037042d614897aa1ee3fb
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-08-26 15:44:48 -0400

    Add some seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,35 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ExConductor.Accounts
+
+users = [
+  %{
+    email: "michael@test.com",
+    username: "michael",
+    password: "password12345"
+  },
+  %{
+    email: "lauren@test.com",
+    username: "lauren",
+    password: "password12345"
+  },
+  %{
+    email: "jeffrey@test.com",
+    username: "jeffrey",
+    password: "password12345"
+  },
+  %{
+    email: "adam@test.com",
+    username: "adam",
+    password: "password12345"
+  },
+  %{
+    email: "admin@test.com",
+    username: "admin",
+    password: "password12345",
+    is_admin: true
+  }
+]
+
+Enum.each(users, &Accounts.register_user/1)
